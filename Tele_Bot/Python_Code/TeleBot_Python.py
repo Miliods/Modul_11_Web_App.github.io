509affee9e1c781f56a0689e38286cf2de4ec26b
miliods <rimdiablo@gmail.com>
2024-05-02 15:56:46 +0500

    Start

--- Tele_Bot/Python_Code/TeleBot_Python.py
+++ Tele_Bot/Python_Code/TeleBot_Python.py
@@ -1,22 +1,16 @@
 import asyncio
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
+from aiogram import Bot
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-
-@dp.message(CommandStart())
-async def start(message: types.Message):
-    await message.answer("hello")
 
 
 async def main() -> None:
+    from TeleBot_Hendlers import dp
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
