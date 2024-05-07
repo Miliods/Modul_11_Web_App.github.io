331398cf524168cdfe0059a391ed8816a2557224
miliods <rimdiablo@gmail.com>
2024-05-07 16:02:02 +0500

    Update

---
+++ javascrip.js
@@ -0,0 +1,17 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#fff";
+tg.MainButton.color = "orange"
+
+let btn1 = document.getElementBuId("btn1")
+
+let btn2 = document.getElementBuId("btn2")
+
+let btn3 = document.getElementBuId("btn3")
+
+
+btn1.addEventListener("click", function(){
+    tg.MainButton.setText("btn1 clicked")
+})
