3c66c3d67921ae17aca7354fbb540d83e19f5dfd
miliods <rimdiablo@gmail.com>
2024-05-09 16:23:49 +0500

    Start

--- javascrip.js
+++ javascrip.js
@@ -14,40 +14,37 @@ let btn4 = document.getElementById("btn4");
 let btn5 = document.getElementById("btn5");
 let btn6 = document.getElementById("btn6");
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Burger qoshildi!");
-		item = "1";
-		tg.MainButton.show();
-	}
+btn1.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Burger qoshildi!");
+        item = "1";
+        tg.MainButton.show();
+    }
 });
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Hot-Dog qosholdi!");
-		item = "2";
-		tg.MainButton.show();
-	}
+btn2.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Hot-Dog qosholdi!");
+        item = "2";
+        tg.MainButton.show();
+    }
 });
 
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Pizza qoshildi!");
-		item = "3";
-		tg.MainButton.show();
-	}
+btn3.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        tg.MainButton.setText("Pizza qoshildi!");
+        item = "3";
+        tg.MainButton.show();
+    }
 });
-Telegram.WebApp.onEvent("mainButtonClicked", function() {
-	tg.sendData("TestMessage");
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
 });
 
 
