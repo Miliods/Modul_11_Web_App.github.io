f5c8669354c9e91c30cc5dfbee818411ff7e9d24
miliods <rimdiablo@gmail.com>
2024-05-09 15:19:41 +0500

    Start

--- Tele_Bot/Python_Code/TeleBot_Hendlers.py
+++ Tele_Bot/Python_Code/TeleBot_Hendlers.py
@@ -1,4 +1,4 @@
-from aiogram import Dispatcher, types
+from aiogram import Dispatcher, types, F
 from aiogram.filters import CommandStart
 
 from Tele_Bot.Python_Code.keyboards import app_kb
@@ -9,4 +9,9 @@ dp = Dispatcher()
 @dp.message(CommandStart())
 async def start(message: types.Message):
     await message.answer("hello", reply_markup=app_kb)
+
+
+@dp.message(F.data)
+async def get_btn(message: types.Message):
+    await message.answer(message.text)
     
