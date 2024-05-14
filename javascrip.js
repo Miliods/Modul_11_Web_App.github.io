e856ee62c0383d04481fa6d98fda7cda3edc3fa1
miliods <rimdiablo@gmail.com>
2024-05-14 16:22:56 +0500

    Start

--- javascrip.js
+++ javascrip.js
@@ -13,87 +13,69 @@ let add_btn2 = document.getElementById("add_btn2");
 let remove_btn3 = document.getElementById("remove_btn3");
 let add_btn3 = document.getElementById("add_btn3");
 
+let item = " ";
+let num_count = 0;
+let num_count1 = 0;
+let num_count2 = 0;
 
-let item = " "
-let num_count = 0
-let num_count1 = 0
-let num_count2 = 0
-
-number = document.getElementById("count");
-number1 = document.getElementById("count1");
-number2 = document.getElementById("count2");
+let number = document.getElementById("count");
+let number1 = document.getElementById("count1");
+let number2 = document.getElementById("count2");
 
 add_btn1.addEventListener("click", function () {
     tg.MainButton.setText("Burger");
     item = "Burger/4.99/" + num_count.innerText;
     tg.MainButton.show();
-    number.innerText = num_count += 1;
+    number.innerText = ++num_count;
     number.style.display = "block";
-})
+});
 
 remove_btn1.addEventListener("click", function () {
     if (num_count > 0) {
-        number.innerText = num_count -= 1;
+        number.innerText = --num_count;
     }
     tg.MainButton.setText("Burger");
     item = "Burger/4.99/" + num_count.innerText;
     tg.MainButton.show();
     number.style.display = "block";
-})
+});
 
 add_btn2.addEventListener("click", function () {
     tg.MainButton.setText("Hot-Dog");
     item = "Hot-Dog/3.99/" + num_count1.innerText;
     tg.MainButton.show();
-    number1.innerText = num_count1 += 1;
+    number1.innerText = ++num_count1;
     number1.style.display = "block";
-})
+});
 
 remove_btn2.addEventListener("click", function () {
     if (num_count1 > 0) {
-        number1.innerText = num_count1 -= 1;
+        number1.innerText = --num_count1;
     }
     tg.MainButton.setText("Hot-Dog");
     item = "Hot-Dog/3.99/" + num_count1.innerText;
     tg.MainButton.show();
 
     number1.style.display = "block";
-})
+});
 
 add_btn3.addEventListener("click", function () {
     tg.MainButton.setText("Pizza");
-    item = "Pizza/5.99" + num_count2.innerText;
+    item = "Pizza/5.99/" + num_count2.innerText;
     tg.MainButton.show();
-    number2.innerText = num_count2 += 1;
+    number2.innerText = ++num_count2;
     number2.style.display = "block";
-})
+});
 
 remove_btn3.addEventListener("click", function () {
     if (num_count2 > 0) {
-        number2.innerText = num_count2 -= 1;
+        number2.innerText = --num_count2;
     }
     tg.MainButton.setText("Pizza");
-    item = "Pizza/5.99" + num_count2.innerText;;
+    item = "Pizza/5.99/" + num_count2.innerText;
     tg.MainButton.show();
     number2.style.display = "block";
-})
-
-// btn1.addEventListener("click", function () {
-    
-//     tg.MainButton.setText("Burger");
-//     item = "Burger/$4.99";
-//     tg.MainButton.show();
-// });
-// btn2.addEventListener("click", function () {
-//     tg.MainButton.setText("Hot-Dog");
-//     item = "Hot-Dog/$3.99";
-//     tg.MainButton.show();
-// });
-// btn4.addEventListener("click", function () {
-//     tg.MainButton.setText("Pizza");
-//     item = "Pizza/$5.99";
-//     tg.MainButton.show();
-// });
+});
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
     tg.sendData(item);
