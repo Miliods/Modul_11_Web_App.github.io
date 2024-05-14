d07163c141bdd1a0a436e5edc6e6646c883ed65a
miliods <rimdiablo@gmail.com>
2024-05-14 16:27:15 +0500

    Start

--- Tele_Bot/Python_Code/TeleBot_Hendlers.py
+++ Tele_Bot/Python_Code/TeleBot_Hendlers.py
@@ -14,21 +14,12 @@ async def start(message: types.Message):
 @dp.message(F.func(lambda msg: msg.web_app_data.data))
 async def get_btn(msg: types.Message):
     text = msg.web_app_data.data
-    parts = text.split('/')
-    if len(parts) != 3:
-        await msg.answer("Invalid data format.")
-        return
-
-    title = parts[0]
-    try:
-        price = float(parts[1])
-        quantity = int(parts[2])
-    except ValueError:
-        await msg.answer("Invalid price or quantity format.")
-        return
-
+    title = text.split('/')[0]
+    price = int(text.split('/')[1])
+    quantity = int(text.split('/')[2])
     await msg.answer(text=f"{title}\n"
                           f"{quantity} pcs \n"
                           f"total cost : {quantity * price}$")
+
 
     
