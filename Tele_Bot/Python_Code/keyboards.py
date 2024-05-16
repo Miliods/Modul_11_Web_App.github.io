57f10ea58257b2456cb5f21e2d9a73d1c8cff072
miliods <rimdiablo@gmail.com>
2024-05-16 15:10:24 +0500

    Start

--- Tele_Bot/Python_Code/keyboards.py
+++ Tele_Bot/Python_Code/keyboards.py
@@ -1,6 +1,9 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
 
 web_app = WebAppInfo(url="https://miliods.github.io/Modul_11_Web_App.github.io/")
 app_kb = ReplyKeyboardMarkup(keyboard=[
     [KeyboardButton(text="Mini app", web_app=web_app)]
 ], resize_keyboard=True)
+buy_ikb = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="Tolov", callback_data='buy')],
+])
